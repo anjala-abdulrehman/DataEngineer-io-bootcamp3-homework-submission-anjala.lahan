5c4caeeefee850c5581ba9f0c64793eace8d3b1c
anjala-abdulrehman <anjalaar20@gmail.com>
2024-01-09 11:49:11 +0400

    Assignment submission dimensional data modelling

--- all_tracks/dimensional_modeling/submission/query_2.sql
+++ all_tracks/dimensional_modeling/submission/query_2.sql
@@ -1,11 +1,5 @@
--- Cumulative Table Computation Query (query_2)
--- Write a query that populates the actors table one year at a time.
--- Below query loads data for the year 2010, increament current_year and YEAR by 1 to add data for subsequent years
-
-INSERT INTO
-  anjala.actors
 WITH
-  last_year AS (
+  last_year AS ( -- last year data
     SELECT
       *
     FROM
@@ -13,61 +7,54 @@ WITH
     WHERE
       current_year = 2009
   ),
-  this_year AS (
+  this_year AS ( -- current year data
     SELECT
       actor,
       actor_id,
       ARRAY_AGG(ROW (film, votes, rating, film_id)) AS films,
-      YEAR AS current_year,
-      YEAR IS NOT NULL AS is_active
+      CASE
+        WHEN AVG(rating)  > 8 THEN 'star'
+        WHEN AVG(rating) > 7 THEN 'good'
+        WHEN AVG(rating) >  6 THEN 'average'
+        ELSE 'bad'
+      END  AS quality_class,
+      True AS is_active, -- data for this year is always present, so for this year is_active is  True
+      YEAR AS current_year
     FROM
       bootcamp.actor_films
     WHERE
       YEAR = 2010
-    GROUP BY
+      GROUP BY
       actor,
       actor_id,
       YEAR
   ),
   combined AS (
     SELECT
-      COALESCE(ty.actor, ly.actor) AS actor,
+      COALESCE(ty.actor, ly.actor) AS actor, -- if actor name for this year is null then get last year name
       COALESCE(ty.actor_id, ly.actor_id) AS actor_id,
+      COALESCE (ty.quality_class, ly.quality_class ) as quality_class,
       CASE
-        WHEN ty.is_active
-        AND ly.is_active THEN ly.films || ty.films
-        WHEN ty.is_active
-        AND NOT ly.is_active THEN ty.films
-        WHEN NOT ty.is_active
-        AND ly.is_active THEN ly.films
-        WHEN ty.is_active
-        AND ly.is_active IS NULL THEN ty.films
+        WHEN ty.is_active AND ly.is_active THEN ly.films || ty.films
+        WHEN ty.is_active AND NOT ly.is_active THEN ty.films
+        WHEN NOT ty.is_active AND ly.is_active THEN ly.films
+        WHEN ty.is_active AND ly.is_active IS NULL THEN ty.films
       END AS films,
-      ty.current_year IS NOT NULL AS is_active,
+      CASE
+        WHEN ly.is_active AND ty.is_active is NULL then false
+        WHEN ty.current_year IS NOT NULL then true
+      END as is_active,
       ty.current_year
     FROM
       this_year ty
-      FULL OUTER JOIN last_year ly ON ty.actor = ly.actor
+      FULL OUTER JOIN last_year ly ON ty.actor_id = ly.actor_id
   )
 SELECT
   c.actor,
   c.actor_id,
   c.films,
-  CASE
-    WHEN AVG(arr.rating) > 8 THEN 'star'
-    WHEN AVG(arr.rating) > 7
-    AND AVG(arr.rating) <= 8 THEN 'good'
-    WHEN AVG(arr.rating) > 6
-    AND AVG(arr.rating) <= 7 THEN 'average'
-    WHEN AVG(arr.rating) <= 6 THEN 'bad'
-  END AS quality_class,
-  MAX(c.is_active) AS is_active,
+  c.quality_class,
+  is_active,
   c.current_year
 FROM
   combined c
-  CROSS JOIN UNNEST (films) AS arr (film, votes, rating, film_id)
-GROUP BY
-  c.actor,
-  c.actor_id,
-  c.films,
-  c.current_year
--- all_tracks/dimensional_modeling/submission/query_3.sql
+++ all_tracks/dimensional_modeling/submission/query_3.sql
@@ -1,12 +1,16 @@
 -- Actors History SCD Table DDL (query_3)
 -- Create a DDL for an actors_history_scd
 
-CREATE TABLE anjala.actors_history_scd 
-  ( actor varchar, 
-  quality_class varchar, 
-  is_active boolean, 
-  start_date integer, 
-  end_date integer ) 
-WITH ( 
-  format = 'PARQUET', 
+CREATE TABLE anjala.actors_history_scd
+  (
+      actor varchar,
+      actor_id varchar,
+      quality_class varchar,
+      is_active boolean,
+      start_date integer,
+      end_date integer,
+      current_year integer
+  )
+WITH (
+  format = 'PARQUET',
   partitioning = ARRAY['start_date'] )
