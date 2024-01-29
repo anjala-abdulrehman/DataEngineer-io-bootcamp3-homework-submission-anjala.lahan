06df7d76d0730e69dd33d201d99fb356a36137db
Anjala A R <115990433+anjala-abdulrehman@users.noreply.github.com>
2024-01-29 10:50:23 +0400

    Update job_2.py

--- infrastructure/data_quality/src/job_2.py
+++ infrastructure/data_quality/src/job_2.py
@@ -2,23 +2,43 @@ from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
-def query_2(output_table_name: str) -> str:
-    return f"""
-    <YOUR QUERY HERE>
+
+def query_2() -> str:
+    return """
+    SELECT
+        player_name AS subject_identifier,
+        'player' AS subject_type,
+        CAST(team_id AS STRING) AS object_identifier,
+        'team' AS object_type,
+        'plays_on' AS edge_type,
+        MAP(
+            'number_of_games', CAST(COUNT(1) AS STRING),
+            'number_of_points', CAST(SUM(pts) AS STRING)
+    ) AS properties
+FROM
+    nba_game_details
+GROUP BY
+    player_name,
+    team_id
     """
 
-def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_2(output_table_name))
+
+def job_2(spark_session: SparkSession, dataframe) -> Optional[DataFrame]:
+    dataframe.createOrReplaceTempView("nba_game_details")
+    return spark_session.sql(query_2())
+
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "nba_player_edges"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
         .appName("job_2")
         .getOrCreate()
     )
-    output_df = job_2(spark_session, output_table_name)
+    output_df = job_2(spark_session, spark_session.table("nba_player_edges"))
     output_df.write.mode("overwrite").insertInto(output_table_name)
+
+
+if __name__ == '__main__':
+    main()
